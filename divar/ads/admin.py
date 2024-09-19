76a0a866028cd8ba5b71b565520bbf62dfd3292c
Your Name <you@example.com>
2024-09-19 09:43:25 -0700

    merge

--- divar/ads/admin.py
+++ divar/ads/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin 
-<<<<<<< HEAD
 from .models import  *
 # Register your models here.
 @admin.register (Ad)
@@ -21,11 +20,3 @@ class NotificationAdmin(admin.ModelAdmin): ...
 
 
 
-=======
-from .models import  Ad , Category
-# Register your models here.
-admin.site.register(Ad)
-admin.site.register(Category)
-# @admin.register (Ad)
-# class AdAdmin(admin.ModelAdmin): ...
->>>>>>> 2908150b93fe6c7e4255bc955da488d5bab21975
